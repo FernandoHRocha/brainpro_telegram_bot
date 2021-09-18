fffb586c3b6127b646bc242e67d768f158b1b548
Fernando Rocha <fhrlobacz@gmail.com>
2021-09-18 08:58:13 -0300

    Criados arquivos iniciais

---
+++ recursos.py
@@ -0,0 +1,3 @@
+#BOT
+nome = 'brainpro_telegram_bot'
+saudacao = "Olá, seja bem - vindo (a) ao Canal da Brainpro no Telegram! Somos especialistas em proporcionar soluções de tecnologia através da venda de produtos de Realidade Virtual e equipamentos de informática. Então falaremos muito sobre esse universo por aqui, esperamos que goste! 🧠"
