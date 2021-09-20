abf666fb71ce9bf166f0f80d55dfb51d87802340
Fernando Rocha <fhrlobacz@gmail.com>
2021-09-20 17:37:35 -0300

    Implementadas categorias iniciais

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import telebot
+from telebot.types import ReplyKeyboardRemove
 import recursos
 import os
 
 bot = telebot.TeleBot(os.environ['BRAINPRO_BOT_TOKEN'], parse_mode=None)
 canal = os.environ['BRAINPRO_ID_CANAL']
 saudacao = recursos.saudacao
+
+produtos =['Aqui estão alguns dos nossos melhores produtos:','Mouse gamer','Teclado mecânico','Estação de trabalho']
 
 @bot.message_handler(func=lambda message: True, content_types=['new_chat_members'])
 def boas_vindas(message):
@@ -13,6 +16,11 @@ def boas_vindas(message):
 
 @bot.message_handler(commands=['start'])
 def enviar_saudacao(message):
-    bot.send_message(message.chat.id,saudacao)
+    bot.send_message(message.from_user.id,saudacao,reply_markup=recursos.markup_saudacao())
+
+@bot.message_handler(lambda m: True)
+def enviar_produtos(message):
+    for produto in produtos:
+        bot.send_message(message.from_user.id,produto,reply_markup=ReplyKeyboardRemove())
 
 bot.polling()
--- recursos.py
+++ recursos.py
@@ -1,3 +1,17 @@
+import telebot
+from telebot import types
 #BOT
 nome = 'brainpro_telegram_bot'
 saudacao = "Olá, seja bem - vindo (a) ao Canal da Brainpro no Telegram! Somos especialistas em proporcionar soluções de tecnologia através da venda de produtos de Realidade Virtual e equipamentos de informática. Então falaremos muito sobre esse universo por aqui, esperamos que goste! 🧠"
+
+def markup_saudacao():
+    """
+    Retorna as opções primárias de categorias.
+    """
+    markup = types.ReplyKeyboardMarkup(row_width=3)
+    markup.add(
+        types.KeyboardButton('Produtos'),
+        types.KeyboardButton('Serviços'),
+        types.KeyboardButton('Oportunidades')
+        )
+    return markup
