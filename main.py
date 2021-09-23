f68fc3cba9123a21e924059bc769bc84868ae194
Fernando Rocha <fhrlobacz@gmail.com>
2021-09-23 17:57:23 -0300

    Implementando itens da loja

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import telebot
-from telebot.types import ReplyKeyboardRemove
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove
 import recursos
 import os
 
 bot = telebot.TeleBot(os.environ['BRAINPRO_BOT_TOKEN'], parse_mode=None)
 canal = os.environ['BRAINPRO_ID_CANAL']
 saudacao = recursos.saudacao
-
-produtos =['Aqui estão alguns dos nossos melhores produtos:','Mouse gamer','Teclado mecânico','Estação de trabalho']
 
 @bot.message_handler(func=lambda message: True, content_types=['new_chat_members'])
 def boas_vindas(message):
@@ -18,9 +16,24 @@ def boas_vindas(message):
 def enviar_saudacao(message):
     bot.send_message(message.from_user.id,saudacao,reply_markup=recursos.markup_saudacao())
 
-@bot.message_handler(func=lambda m: True)
-def enviar_produtos(message):
-    for produto in produtos:
-        bot.send_message(message.from_user.id,produto,reply_markup=ReplyKeyboardRemove())
+@bot.message_handler(func=lambda m: m.text == "Produtos")
+def enviar_produtos(m):
+    bot.send_message(m.chat.id,'Escolha uma categoria de produtos.',reply_markup=recursos.markup_categorias_produto())
+
+@bot.message_handler(func=lambda m: m.text == "Placa de video")
+def categoria_placa_video(m):
+    bot.send_message(m.chat.id,'Os nossos produtos são:',reply_markup=recursos.markup_vazio())
+    enviar_produtos(m)
+
+def enviar_produtos(m):
+    for produto in recursos.retorno['retorno']['produtos']:
+        marca = produto['produto']['marca']
+        descricao = produto['produto']['descricao']
+        imagem = produto['produto']['imagem'][0]['link']
+        preco = str('R$ '+str(round(float(produto['produto']['preco']),2)))
+        bot.send_photo(m.chat.id,imagem,marca+'\n'+descricao+'\n'+preco,reply_markup=recursos.markup_site())
+
+def printar():
+    print('produto')
 
 bot.polling()
--- recursos.py
+++ recursos.py
@@ -1,13 +1,69 @@
 import telebot
 from telebot import types
+
 #BOT
 nome = 'brainpro_telegram_bot'
 saudacao = "Olá, seja bem - vindo (a) ao Canal da Brainpro no Telegram! Somos especialistas em proporcionar soluções de tecnologia através da venda de produtos de Realidade Virtual e equipamentos de informática. Então falaremos muito sobre esse universo por aqui, esperamos que goste! 🧠"
 
+#DADOS
+retorno={  
+   "retorno":{  
+      "produtos":[  
+         {
+            "produto":{  
+               "codigo":"001",
+               "descricao":"RX 550 4GB GDDR5 128 BITS SINGLE-FAN",
+               "preco":"2253.1000000000",
+               "marca":"AMD Radeon",
+               "imagem":[
+                  {
+                    "link": "https://cdn.awsli.com.br/1000x1000/1628/1628166/produto/122163329/c0ba90a69c.jpg",
+                  }],
+               "estoqueAtual":2,
+            }
+        },{
+            "produto":{  
+               "codigo":"002",
+               "descricao":"RX 560 4GB GDDR5 128 BITS",
+               "preco":"2024.0000000000",
+               "marca":"AMD Radeon",
+               "imagem":[
+                  {
+                    "link": "https://cdn.awsli.com.br/1000x1000/1628/1628166/produto/122162448/1fdaad202b.jpg",
+                  }],
+               "estoqueAtual":3,
+            }
+        },{
+            "produto":{  
+               "codigo":"003",
+               "descricao":"RTX 3060 TI 8GB GDDR6 256 BITS",
+               "preco":"9649.0000000000",
+               "marca":"NVIDIA GeForce",
+               "imagem":[
+                  {
+                    "link": "https://cdn.awsli.com.br/1000x1000/1628/1628166/produto/118794486/42240f583b.jpg",
+                  }],
+               "estoqueAtual":1,
+            }
+        }
+    ]
+   }
+}
+
+def markup_vazio():
+    return types.ReplyKeyboardRemove()
+
+def markup_site():
+    """Retorna um botão um com o link da loja."""
+    keyboard = [
+    [
+        types.InlineKeyboardButton("Brainpro Tecnologia",url='www.brainpro.com.br'),
+    ]
+    ]
+    return types.InlineKeyboardMarkup(keyboard)
+
 def markup_saudacao():
-    """
-    Retorna as opções primárias de categorias.
-    """
+    """Retorna as opções primárias de categorias."""
     markup = types.ReplyKeyboardMarkup(row_width=3)
     markup.add(
         types.KeyboardButton('Produtos'),
@@ -15,3 +71,14 @@ def markup_saudacao():
         types.KeyboardButton('Oportunidades')
         )
     return markup
+
+def markup_categorias_produto():
+    """Retorna as categorias de produtos."""
+    markup = types.ReplyKeyboardMarkup(row_width=3)
+    markup.add(
+        types.KeyboardButton('Placa de video'),
+        types.KeyboardButton('Monitor'),
+        types.KeyboardButton('Periféricos')
+        )
+    return markup
+
